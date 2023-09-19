9674cdabd522dea03b3f08634d005f3079b1e321
Rohan Sanjay Mahajan <rohansanjay.mahajan@kaplan.edu>
2023-09-19 10:37:11 +0530

    Implemented CRUD operation - Added PUT HTTP Method - Added POST HTTP Method - Added Database Query - Refactored the code

--- books/DataBase Query/RetrieveBooksFromDatabase.sql
+++ books/DataBase Query/RetrieveBooksFromDatabase.sql
@@ -1,3 +1,3 @@
-/* Query to Retrieve all book from database /*
+-- Query to Retrieve all book from database 
 
 SELECT b.book_id, b.title, a.author_name, p.publisher_name, p.published_date, b.description FROM Books b INNER JOIN Authors a ON b.author_id = a.author_id INNER JOIN Publishers p ON b.publisher_id = p.publisher_id WHERE b.book_id = @BookId;
--- books/Services/BooksService.cs
+++ books/Services/BooksService.cs
@@ -23,11 +23,16 @@ namespace books.Services
            
         }
 
+
+        /// <summary>
+        /// FetchBooksFromApiAsync() method reads the values from the api call and Stores it in the database
+        /// </summary>
+        /// <returns></returns>
         public async Task<List<BookInfo>> FetchBooksFromApiAsync()
         {
             var httpClient = new HttpClient();
              var apiUrl = "https://www.googleapis.com/books/v1/volumes?q=kaplan%20test%20prep";
-            // var apiUrl = "https://www.googleapis.com/books/v1fgfg";
+             //var apiUrl = "https://www.googleapis.com/books/v1fgfg";
             try
             {
                 var response = await httpClient.GetAsync(apiUrl);
@@ -47,7 +52,7 @@ namespace books.Services
 
                                 Author = item.volumeInfo.authors != null ? string.Join(", ", item.volumeInfo.authors) : "No author",
                                 Title = item.volumeInfo.title,
-                                Publisher = item.volumeInfo.publisher,
+                                Publisher = !string.IsNullOrEmpty(item.volumeInfo.publisher) ? item.volumeInfo.publisher : "NA",
                                 PublishedDate = item.volumeInfo.publishedDate,
                                 Description = item.volumeInfo.description
 
@@ -71,6 +76,10 @@ namespace books.Services
             return new List<BookInfo>();
         }
 
+        /// <summary>
+        /// RetrieveBooksFromJson() method reads the values from the JSON file and Stores it in the database
+        /// </summary>
+        /// <returns></returns>
         public async Task<List<BookInfo>> RetrieveBooksFromJson()
         {
             var jsonFilePath = @"C:\Users\RMahajan\Desktop\BooksAPI_1\books\Controllers\books.json";
@@ -107,6 +116,10 @@ namespace books.Services
 
         }
 
+        /// <summary>
+        /// SeedDatabaseAsync() method does the function calls for FetchBooksFromApiAsync(),RetrieveBooksFromJson(),RetrieveBooksFromDatabase() and this method is called in HTTP GET request
+        /// </summary>
+        /// <returns></returns>
         public async Task<List<BookInfo>> SeedDatabaseAsync()
         {
             List<Book> booksFromDatabase = _databaseService.RetrieveBooksFromDatabase();
@@ -163,40 +176,83 @@ namespace books.Services
             }
         }
 
+
+        /// <summary>
+        /// GetBooksFromDatabase() method retrieves the value from database
+        /// </summary>
+        /// <returns></returns>
         public List<Book> GetBooksFromDatabase()
         {
             var booksFromDatabase = _databaseService.RetrieveBooksFromDatabase();
 
             if (booksFromDatabase.Count == 0)
             {
-                return new List<Book>(); // Return an empty list to indicate no records were found
+                return new List<Book>(); 
             }
             else
             {
-                return booksFromDatabase; // Return the books from the database
+                return booksFromDatabase; 
             }
         }
 
+
+        /// <summary>
+        ///  GetBookById method uses paramater bookId and returns the values of respective bookId
+        /// </summary>
+        /// <param name="bookId"></param>
+        /// <returns></returns>
         public Book GetBookById(int bookId)
         {
 
             return _databaseService.RetrieveBookFromDatabase(bookId);
         }
 
+        /// <summary>
+        ///  DeleteBookById method uses paramater bookId and deletes the values of respective bookId
+        /// </summary>
+        /// <param name="bookId"></param>
+        /// <returns></returns>
         public bool DeleteBookById(int bookId)
         {
             return _databaseService.DeleteBookByIdFromDatabase(bookId);
         }
 
-        List <Book> IBookService.RetrieveBooksFromDatabase()
+
+        /// <summary>
+        /// RetrieveBooksFromDatabase returns an method of IDatabaseService
+        /// </summary>
+        /// <returns></returns>
+        List<Book> IBookService.RetrieveBooksFromDatabase()
         {
             return _databaseService.RetrieveBooksFromDatabase();
         }
 
-         public Task StoreBooksInDatabase(List<BookInfo> bookInfos)
+        /// <summary>
+        /// StoreBooksInDatabase returns an method of IDatabaseService
+        /// </summary>
+        /// <returns></returns>
+        public Task StoreBooksInDatabase(List<BookInfo> bookInfos)
         {
             return _databaseService.StoreBooksInDatabase(bookInfos);
         }
-      
+
+        /// <summary>
+        /// PostintoBooksTable returns an method of IDatabaseService
+        /// </summary>
+        /// <returns></returns>
+
+        public BookInfoModel PostintoBooksTable(BookInfoModel bookInfo)
+        {
+            return _databaseService.PostIntoBooks(bookInfo);
+        }
+
+        /// <summary>
+        /// PutintoBooksTable returns an method of IDatabaseService
+        /// </summary>
+        /// <returns></returns>
+        public BookInfoModel PutintoBooksTable(int bookId, BookInfoModel bookInfo)
+        {
+            return _databaseService.PutIntoBooks(bookId, bookInfo);
+        }
     }
 }
